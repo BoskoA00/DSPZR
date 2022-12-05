b0da3480e68b43636b7071e0a959aabc3817b17a
Bosko Amanovic <92599818+BoskoA00@users.noreply.github.com>
2022-12-05 17:34:22 +0100

    Add files via upload

---
+++ PZR/RestController.php
@@ -0,0 +1,36 @@
+<?php
+require_once("AutoRestHandler.php");
+		
+$view = "";
+if(isset($_GET["view"]))
+	$view = $_GET["view"];
+
+switch($view){
+
+	case "all":
+		//  /auto/list/
+		$AutoRestHandler = new AutoRestHandler();
+		$AutoRestHandler->sviAutomobili();
+		break;
+		
+	case "single":
+		// /auto/list/<id>/
+		$AutoRestHandler = new AutoRestHandler();
+		$AutoRestHandler->jedanAuto($_GET["id"]);
+		break;
+		
+	case "delete":
+		// /auto/delete/<id>/
+		$AutoRestHandler = new AutoRestHandler();
+		$AutoRestHandler->izbrisiAuto($_GET["id"]);
+		break;
+	case "update":
+		// /auto/update/<id>/
+		$AutoRestHandler = new AutoRestHandler();
+		$AutoRestHandler->updateAuto($_GET["id"]);
+		break;
+	case "" :
+		//404 - not found;
+		break;
+}
+?>
